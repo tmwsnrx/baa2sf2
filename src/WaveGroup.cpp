b24aabe3bc21bbc093e4038522e9b7c7a5103c2e
tmwsnrx <xrnswmt@protonmail.com>
2025-01-04 19:36:00 +0100

    Add missing includes

--- src/WaveGroup.cpp
+++ src/WaveGroup.cpp
@@ -4,25 +4,27 @@
 #include "WaveBank.hpp"
 #include "WaveHandle.hpp"
 
+#include <cstdint>
+
 namespace z2sound
 {
 
-WaveGroup::WaveGroup(WaveBank& wave_bank, size_t num_entries)
-: wave_bank_{std::ref(wave_bank)}
-{
-  group_entries.reserve(num_entries);
-  Entry default_entry{ .wave_id = 0xffff };
-  std::fill(group_entries.begin(), group_entries.end(), default_entry);
-}
+  WaveGroup::WaveGroup(WaveBank &wave_bank, size_t num_entries)
+      : wave_bank_{std::ref(wave_bank)}
+  {
+    group_entries.reserve(num_entries);
+    Entry default_entry{.wave_id = 0xffff};
+    std::fill(group_entries.begin(), group_entries.end(), default_entry);
+  }
 
-const std::string& WaveGroup::get_filename() const
-{
-  return filename_;
-}
+  const std::string &WaveGroup::get_filename() const
+  {
+    return filename_;
+  }
 
-void WaveGroup::set_filename(std::string&& filename)
-{
-  filename_ = filename;
-}
+  void WaveGroup::set_filename(std::string &&filename)
+  {
+    filename_ = filename;
+  }
 
 }
