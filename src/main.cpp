792db17d7c7fc72cd53a89b36e16d77af7876061
tmwsnrx <xrnswmt@protonmail.com>
2025-01-04 19:49:08 +0100

    Add converting all wave banks

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char** argv)
 
   if (!sound_file)
   {
-    std::cout << "Could not open " << baaPath << std::endl;
+      logger.error("Could not open %s", baaPath.string());
   }
 
   z2sound::AudioArchiveLoader loader{sound_file, logger};
@@ -37,70 +37,55 @@ int main(int argc, char** argv)
 
   if (!audio_archive)
   {
-    std::cout << "Failed to load audio archive" << std::endl;
+      logger.error("Failed to load audio archive");
   }
 
-  const uint8_t wave_bank_no = 1;
+  logger.information("Archive has %u wave banks", static_cast<unsigned>(audio_archive->wave_banks_.size()));
 
-  auto bgm_bank_iterator = audio_archive->wave_banks_.find(wave_bank_no);
-  z2sound::WavePool wave_pool{bgm_bank_iterator->second};
-  wave_pool.set_base_directory(waveFolder);
-
-  for (auto& group : bgm_bank_iterator->second.get_wave_groups())
+  for (auto& [wave_bank_id, wave_bank] : audio_archive->wave_banks_)
   {
-    logger.information("Loading %s", group.get_filename());
-    wave_pool.load_group(group);
+    logger.information("Wave bank %u has %u wave groups",
+                       static_cast<unsigned>(wave_bank_id), static_cast<unsigned>(wave_bank.get_wave_groups().size()));
+
+      z2sound::WavePool wave_pool{wave_bank};
+      wave_pool.set_base_directory(waveFolder);
+
+      for (auto& group : wave_bank.get_wave_groups())
+      {
+          logger.information("Loading %s", group.get_filename());
+          wave_pool.load_group(group);
+      }
+
+      logger.information("Finished loading wave groups");
+
+      BaaConverter baa_converter{*audio_archive};
+
+      logger.information("Converting wave bank %u to SoundFont...", static_cast<unsigned>(wave_bank_id));
+      auto sf2 = baa_converter.to_sf2(wave_bank_id);
+
+      if (!sf2.has_value())
+      {
+          logger.error("Failed converting wave bank %u to SoundFont", static_cast<unsigned>(wave_bank_id));
+          return -1;
+      }
+
+      logger.information("Done.");
+
+      std::ostringstream filename_stream;
+      filename_stream << baaPath.stem().string() << "_" << std::to_string(wave_bank_id) << ".sf2";
+      std::filesystem::path outputFile = outputFolder / filename_stream.str();
+
+      std::ofstream sf2_file{outputFile, std::ios::binary};
+
+      if (!sf2_file)
+      {
+          logger.error("Could not open output file %s for writing", outputFile.string());
+          return -1;
+      }
+
+      sf2->Write(sf2_file);
+      logger.information("Finished writing " + outputFile.string());
   }
-
-  logger.information("Finished loading wave groups");
-
-  BaaConverter baa_converter{*audio_archive};
-  auto sf2 = baa_converter.to_sf2(wave_bank_no);
-
-  if (!sf2.has_value())
-  {
-    logger.error("Failed converting wave bank %u to SoundFont", static_cast<unsigned>(wave_bank_no));
-    return -1;
-  }
-
-  std::ostringstream filename_stream;
-    filename_stream << baaPath.stem().string() << "_" << std::to_string(wave_bank_no) << ".sf2";
-  std::filesystem::path outputFile = outputFolder / filename_stream.str();
-
-  std::ofstream sf2_file{outputFile, std::ios::binary};
-
-  if (!sf2_file)
-  {
-    logger.error("Could not open output file %s for writing", outputFile.string());
-    return -1;
-  }
-
-  sf2->Write(sf2_file);
-  logger.information("Finished writing " + outputFile.string());
-
-  // for (const auto& bank : audio_archive->instrument_banks_)
-  // {
-  //   if (bank.second.get_wave_bank_id() != wave_bank_no)
-  //   {
-  //     continue;
-  //   }
-
-  //   BaaConverter baa_converter{*audio_archive};
-  //   uint8_t bank_id = bank.second.get_id();
-  //   auto sf2 = baa_converter.to_sf2(bank_id);
-
-  //   if (!sf2.has_value())
-  //   {
-  //     logger.error("Failed converting bank %u to SoundFont", static_cast<unsigned>(bank_id));
-  //     return -1;
-  //   }
-
-  //   auto outputFile = "TP_Bank_" + std::to_string(bank_id) + ".sf2";
-
-  //   std::ofstream sf2_file{outputFile, std::ios::binary};
-  //   sf2->Write(sf2_file);
-  //   logger.information("Finished writing " + outputFile);
-  // }
 }
 
 Poco::Logger& create_logger()
